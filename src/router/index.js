d0df2d3f91e004723e165e7509898d64338cca98
FRACE <frace@inmoods.com>
2020-12-06 21:35:31 +0800

    feat: 登录头像

--- src/router/index.js
+++ src/router/index.js
@@ -1,14 +1,12 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
-import Home from '../views/Home.vue'
 
 Vue.use(VueRouter)
 
 const routes = [
   {
     path: '/',
-    name: 'Home',
-    component: Home
+    redirect: '/login'
   },
   {
     path: '/about',
@@ -16,7 +14,14 @@ const routes = [
     // route level code-splitting
     // this generates a separate chunk (about.[hash].js) for this route
     // which is lazy-loaded when the route is visited.
-    component: () => import(/* webpackChunkName: "about" */ '../views/About.vue')
+    component: () =>
+      import(/* webpackChunkName: "about" */ '../views/About.vue')
+  },
+  {
+    path: '/login',
+    name: 'login',
+    component: () =>
+      import(/* webpackChunkName: "login" */ '../views/Login.vue')
   }
 ]
 
---
+++ src/views/Login.vue
@@ -0,0 +1,51 @@
+<template>
+  <div class="login_container">
+    <div class="login_box">
+      <div class="avatar_box">
+        <img src="../assets/logo.png" alt="" />
+      </div>
+    </div>
+  </div>
+</template>
+<script>
+export default {
+  data () {
+    return {}
+  }
+}
+</script>
+<style lang="less" scoped>
+.login_container {
+  background: #2b4b6b;
+  height: 100vh;
+  display: flex;
+  justify-content: center;
+  align-items: center;
+}
+.login_box {
+  background: #fff;
+  height: 300px;
+  width: 450px;
+  // position: absolute;
+  // left: 50%;
+  // top: 50%;
+  // transform: translate(-50%, -50%);
+}
+.avatar_box {
+  height: 130px;
+  width: 130px;
+  border: 1px solid #eee;
+  border-radius: 50%;
+  padding: 10px;
+  margin: 0 auto;
+  box-shadow: 0 0 10px #ddd;
+  transform: translateY(-50%);
+  background: #fff;
+  img {
+    width: 100%;
+    height: 100%;
+    border-radius: 50%;
+    background: #ddd;
+  }
+}
+</style>
